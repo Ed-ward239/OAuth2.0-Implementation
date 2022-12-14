6a15b4e759165589aef27e97618522dc3e60b1b4
Edward Lee <edward9ed@gmail.com>
2022-12-14 11:26:28 -0500

    Final Commit

---
+++ OAuth_Testing/Program.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Exchange.WebServices.Data;
+using Microsoft.Identity.Client;
+using System;
+using System.Configuration;
+using Microsoft.Azure.Storage.File;
+
+namespace EwsOAuth
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            // Using Microsoft.Identity.Client 4.22.0
+            var cca = ConfidentialClientApplicationBuilder
+                .Create(ConfigurationManager.AppSettings["appId"])
+                .WithClientSecret(ConfigurationManager.AppSettings["clientSecret"])
+                .WithTenantId(ConfigurationManager.AppSettings["tenantId"])
+                .Build();
+
+            var ewsScopes = new string[] { "https://outlook.office365.com/.default" };
+
+            try
+            {
+                var authResult = await cca.AcquireTokenForClient(ewsScopes).ExecuteAsync();
+
+                // Configure the ExchangeService with the access token
+                var ewsClient = new ExchangeService();
+                ewsClient.Url = new Uri("https://outlook.office365.com/EWS/Exchange.asmx");
+                ewsClient.Credentials = new OAuthCredentials(authResult.AccessToken);
+                ewsClient.ImpersonatedUserId =
+                    new ImpersonatedUserId(ConnectingIdType.SmtpAddress, "EdwardLee@NYPDTESTACC.onmicrosoft.com");
+
+                //Include x-anchormailbox header
+                ewsClient.HttpHeaders.Add("X-AnchorMailbox", "EdwardLee@NYPDTESTACC.onmicrosoft.com");
+
+                // Make an EWS call
+                var folders = ewsClient.FindFolders(WellKnownFolderName.MsgFolderRoot, new FolderView(10));
+                foreach (var folder in folders)
+                {
+                    Console.WriteLine($"Folder: {folder.DisplayName}");
+                }
+            }
+            catch (MsalException ex)
+            {
+                Console.WriteLine($"Error acquiring access token: {ex}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex}");
+            }
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.WriteLine("Hit any key to exit...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
